94f4f7859505f46545ff8a41389a0e5185517326
Thomas MAYNADIE <54234406+Tomenol@users.noreply.github.com>
2021-10-16 16:44:12 +0200

    Update accelerometer_gyro_readings.ino

--- Prométhée/Resources/Sensors/Arduino code/MPU6050/accelerometer_gyro_readings/accelerometer_gyro_readings.ino
+++ Prométhée/Resources/Sensors/Arduino code/MPU6050/accelerometer_gyro_readings/accelerometer_gyro_readings.ino
@@ -13,39 +13,39 @@ unsigned long end_time = 0, start_time = 0;
 
 void setup() 
 {
-  Serial.begin(115200);
-  Wire.begin();
-  I2C::begin();
+    Serial.begin(115200);
+    Wire.begin();
+    I2C::begin();
 
-  accelerometer1.initialize();
-  accelerometer2.initialize();
+    accelerometer1.initialize();
+    accelerometer2.initialize();
 }
 
 void loop() 
 {
-  end_time = start_time;
-  start_time = micros();
-  
-  float *accel1_accel_vec = accelerometer1.getRawAccelerationVector();
-  float *accel1_angvel_vec = accelerometer1.getRawAngularVelocityVector();
-  float *accel2_accel_vec = accelerometer2.getRawAccelerationVector();
-  float *accel2_angvel_vec = accelerometer2.getRawAngularVelocityVector();
+    end_time = start_time;
+    start_time = micros();
 
-  // print data format -> t ax1 ax2 ay1 ay2 az1 az2 wx1 wx2 wy1 wy2 wz1 wz2
-  Serial.println(0xFFED, DEC);
-  Serial.println(start_time, DEC);
-  
-  for(uint8_t i = 0; i < 3; i++)
-  {
-    Serial.println(accel1_accel_vec[i], DEC);
-    Serial.println(accel2_accel_vec[i], DEC);
-  }
+    float *accel1_accel_vec = accelerometer1.getRawAccelerationVector();
+    float *accel1_angvel_vec = accelerometer1.getRawAngularVelocityVector();
+    float *accel2_accel_vec = accelerometer2.getRawAccelerationVector();
+    float *accel2_angvel_vec = accelerometer2.getRawAngularVelocityVector();
 
-  for(uint8_t i = 0; i < 3; i++)
-  {
-    Serial.println(accel1_angvel_vec[i], DEC);
-    Serial.println(accel2_angvel_vec[i], DEC);
-  }
+    // print data format -> t ax1 ax2 ay1 ay2 az1 az2 wx1 wx2 wy1 wy2 wz1 wz2
+    Serial.println(0xFFED, DEC);
+    Serial.println(start_time, DEC);
 
-  Serial.println(0xFFEC, DEC);
+    for(uint8_t i = 0; i < 3; i++)
+    {
+        Serial.println(accel1_accel_vec[i], DEC);
+        Serial.println(accel2_accel_vec[i], DEC);
+    }
+
+    for(uint8_t i = 0; i < 3; i++)
+    {
+        Serial.println(accel1_angvel_vec[i], DEC);
+        Serial.println(accel2_angvel_vec[i], DEC);
+    }
+
+    Serial.println(0xFFEC, DEC);
 }
