261288f404934799d415a6ea9e179b4d76ead732
Thomas MAYNADIE <54234406+Tomenol@users.noreply.github.com>
2021-10-16 16:52:38 +0200

    Update pressure_temperature_example.ino

--- Resources/Sensors/Arduino code/BMP180/pressure_temperature_example.ino
+++ Resources/Sensors/Arduino code/BMP180/pressure_temperature_example.ino
@@ -8,27 +8,27 @@ uint32_t t;
 BMP180 bmp180;
 
 void setup() {
-  Serial.begin(4800);
-  I2C::begin();
+    Serial.begin(4800);
+    I2C::begin();
 
-  delay(500);
-  bmp180.initialize();
-  
-  t = 0;
+    delay(500);
+    bmp180.initialize();
+
+    t = 0;
 }
 
 void loop()
 {
-  int32_t pressure, temperature;
-  bmp180.get_temperature_and_pressure(&temperature, &pressure);
-  
-  Serial.print("pressure : ");
-  Serial.print(pressure, DEC);
-  
-  Serial.print(", temperature : ");
-  Serial.println(temperature, DEC);
+    int32_t pressure, temperature;
+    bmp180.get_temperature_and_pressure(&temperature, &pressure);
 
-  delay(1000);
+    Serial.print("pressure : ");
+    Serial.print(pressure, DEC);
 
-  t += 1;
+    Serial.print(", temperature : ");
+    Serial.println(temperature, DEC);
+
+    delay(1000);
+
+    t += 1;
 }
