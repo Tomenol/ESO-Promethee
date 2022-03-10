5b58d4d29c4f9bac99fe8d5faafd7581d0e6074f
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2022-03-10 19:11:57 +0100

    Update aquisition_module.ino

--- Data aquisition module/aquisition_module/aquisition_module.ino
+++ Data aquisition module/aquisition_module/aquisition_module.ino
@@ -31,6 +31,8 @@ double altitude_pressure;
 //         IMUs : MPU9250
 // *********************************
 
+uint32_t t_start;
+
 // IMU sensor objects
 MPU9250 mpu9250_1(0x69);
 MPU9250 mpu9250_2(0x68);
@@ -48,6 +50,7 @@ double accel2[3], gyro2[3], mag_meas2[3];
 double gps_longitude, gps_latitude, gps_altitude;
 int time_hour, time_minute, time_second;
 int time_day, time_month, time_year;
+uint8_t new_gps_data;
 
 // gnss status / quality data
 char *nav_system;
@@ -81,7 +84,7 @@ void setup()
   // attendre fix du GPS pour démarrer
   // set start time to GPS time
   // set start altitude to GPS altitude
-  
+  n_sat = 0;
   // init MPU 9250 chips
   mpu9250_1.initialize();
   mpu9250_2.initialize();
@@ -97,6 +100,10 @@ void setup()
 
 void loop() 
 {
+  t_start = millis();
+  
+  new_gps_data = 0;
+
   if(gnss_receiver.read_gps())
   {
     if(gnss_receiver.is_valid())
@@ -107,22 +114,8 @@ void loop()
       gnss_receiver.get_quality(&nav_system, &n_sat, &hdop);
 
       Serial.println("GPS DATA : ");
-      
-      Serial.println(gps_longitude);
-      Serial.println(gps_latitude);
-      Serial.println(gps_altitude);
-    
-      Serial.println(time_hour);
-      Serial.println(time_minute);
-      Serial.println(time_second);
-    
-      Serial.println(time_year);
-      Serial.println(time_month);
-      Serial.println(time_day);
-    
-      Serial.println(nav_system);
-      Serial.println(n_sat);
-      Serial.println(hdop);
+
+      new_gps_data = 1;
     }
   }
   
@@ -137,46 +130,88 @@ void loop()
   mpu9250_2.getRawAngularVelocityVector(gyro2);
   mpu9250_2.getRawMagVector(mag_meas2);
   
-  Serial.println("\nOTHER DATA : ");
-
+  //Serial.println("\n 0THER DATA : ");
+  Serial.print("$");
+  Serial.print(t_start);
+  Serial.print(";");
   Serial.print(accel1[0]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(accel1[1]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(accel1[2]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(accel2[0]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(accel2[1]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(accel2[2]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(gyro1[0]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(gyro1[1]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(gyro1[2]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(gyro2[0]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(gyro2[1]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(gyro2[2]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(mag_meas1[0]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(mag_meas1[1]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(mag_meas1[2]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(mag_meas2[0]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(mag_meas2[1]);
-  Serial.print("; ");
+  Serial.print(";");
   Serial.print(mag_meas2[2]);
-  Serial.print("; ");
+  Serial.print(";");
 
   Serial.print(pressure);
+  Serial.print(";");
   Serial.print(temperature);
+  Serial.print(";");
   Serial.print(altitude_pressure);
+  Serial.print(";");
+
+  Serial.print(gps_longitude);
+  Serial.print(";");
+  Serial.print(gps_latitude);
+  Serial.print(";");
+  Serial.print(gps_altitude);
+  Serial.print(";");
+//Velocity    
+//Direction
+/*
+  Serial.print(time_hour, DEC);
+  Serial.print("; ");
+  Serial.print(time_minute, DEC);
+  Serial.print("; ");
+  Serial.print(time_second, DEC);
+  Serial.print("; ");
+*/
+  Serial.print(hdop);
+  Serial.print(";");
+  Serial.print(n_sat, DEC);
+  Serial.print(";");
+  Serial.print(new_gps_data);
+  Serial.print(";");
+  Serial.print(gnss_receiver.is_valid());
+  Serial.println("*");
+  /*  
+  Serial.print(nav_system);
+  Serial.print("; ");
+  */
+
+  wait(100);
+}
+
+void wait(uint32_t _time)
+{
+  uint32_t t_stop = t_start + _time;
+  while(millis() < t_stop);
 }
